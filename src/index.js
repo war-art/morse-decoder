ea99f4c94868b0ec90159169068f41e8f67560f0
war-art <70770404+war-art@users.noreply.github.com>
2020-10-16 12:01:39 +0500

    done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let a = [];
+    let str = '';
+    let count = 0;
+
+    for (let ch of expr) {
+        str += ch; 
+        count++;
+        if (count === 10 ){
+            a.push(str);
+            str= '';
+            count = 0;
+        }
+    }
+    return a.map (item =>{
+        let letter = '';
+        if (item === '**********'){
+            letter = ' ';
+            return letter 
+        }
+        for (let i = 0; i <= item.length; i = i + 2 ){
+            if (item[i] === '1' && item [i+1] === '1') letter += '-';
+            if (item [i] === '1' && item[i+1] === '0') letter +='.';
+        }
+        return letter;
+    }).map(letter =>{
+        if (letter === ' ') return' ';
+        if (letter in MORSE_TABLE){
+            return MORSE_TABLE[letter]
+        }
+    }).join('');
 }
 
 module.exports = {
